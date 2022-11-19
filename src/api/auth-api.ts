b03d5c742583522f769abd9bb73f3feb75f77b52
Sergey <bonadysev@gmail.com>
2022-11-19 17:10:28 +0300

    fix 19-11

--- src/api/auth-api.ts
+++ src/api/auth-api.ts
@@ -2,8 +2,8 @@ import axios from 'axios'
 import {LoginResponseType} from "../bll/authReducer";
 
 const instance = axios.create({
-    // baseURL: 'http://localhost:7542/2.0/',
-    baseURL: 'https://neko-back.herokuapp.com/2.0/',
+    baseURL: 'http://localhost:7542/2.0/',
+    // baseURL: 'https://neko-back.herokuapp.com/2.0/',
     // baseURL: process.env.REACT_APP_BACK_URL || 'http://localhost:7542/2.0/' ,
     withCredentials: true
 })
