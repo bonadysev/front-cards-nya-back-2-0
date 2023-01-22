120665bbffd2cbb81ff2b61520f6c9227b1ff1f0
Sergey <bonadysev@gmail.com>
2023-01-22 17:41:15 +0300

    fix index

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import {HashRouter} from "react-router-dom";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import App from './app/App';
 import {Provider} from 'react-redux';
 import {store} from './bll/store';
+import ReactDOM from 'react-dom/client';
 
-ReactDOM.render(
+// ReactDOM.render(
+//     <HashRouter>
+//         <Provider store={store}>
+//             <App/>
+//         </Provider>
+//     </HashRouter>,
+//     document.getElementById('root')
+// );
+
+const root = ReactDOM.createRoot(
+    document.getElementById('root') as HTMLElement
+);
+root.render(
     <HashRouter>
         <Provider store={store}>
-            <App/>
+        <App />
         </Provider>
     </HashRouter>,
-    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
