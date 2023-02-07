a588c212257f034554fa093d1374e590a037449f
Sergey <bonadysev@gmail.com>
2023-02-07 18:47:17 +0300

    CRUD (create new pack, delete pack)

--- src/features/Packs/PackTable3.tsx
+++ src/features/Packs/PackTable3.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import {alpha} from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
@@ -10,14 +9,8 @@ import TablePagination from '@mui/material/TablePagination';
 import TableRow from '@mui/material/TableRow';
 import TableSortLabel from '@mui/material/TableSortLabel';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-import Tooltip from '@mui/material/Tooltip';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Switch from '@mui/material/Switch';
-import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import {visuallyHidden} from '@mui/utils';
 import {useAppDispatch, useAppSelector} from "../../bll/store";
@@ -73,17 +66,7 @@ function EnhancedTableHead(props: any) {
     return (
         <TableHead>
             <TableRow>
-                <TableCell padding="checkbox">
-                    <Checkbox
-                        color="primary"
-                        indeterminate={numSelected > 0 && numSelected < rowCount}
-                        checked={rowCount > 0 && numSelected === rowCount}
-                        onChange={onSelectAllClick}
-                        inputProps={{
-                            'aria-label': 'select all desserts',
-                        }}
-                    />
-                </TableCell>
+                <TableCell padding="checkbox"></TableCell>
                 {headCells.map((headCell) => (
                     <TableCell
                         key={headCell.id}
@@ -119,7 +102,7 @@ function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
     const dispatch = useAppDispatch()
     const {numSelected} = props;
 
-    const addNewPackHandler = (newPack:string) =>{
+    const addNewPackHandler = (newPack: string) => {
         dispatch(addNewPackTC(newPack))
     }
 
@@ -129,7 +112,8 @@ function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
                 <BasicSelect/>
                 <StateTextFields/>
                 <RangeSlider/>
-                <Button variant="contained" sx={{flexGrow: 0, marginLeft: "auto"}} onClick={()=>addNewPackHandler('-NewPack-')}>Add new pack</Button>
+                <Button variant="contained" sx={{flexGrow: 0, marginLeft: "auto"}}
+                        onClick={() => addNewPackHandler('-NewPack-')}>Add new pack</Button>
                 <IconButton>
                     <FilterListIcon/>
                 </IconButton>
@@ -141,9 +125,9 @@ function EnhancedTableToolbar(props: EnhancedTableToolbarProps) {
 export default function PackTable3() {
     const dispatch = useAppDispatch()
     const packs = useAppSelector(state => state.pack.cardPacks)
-    const pageCount = useAppSelector(state => state.pack.pageCount)
-    const page = useAppSelector(state => state.pack.page)
-    const cardsPacksTotalCount = useAppSelector(state => state.pack.cardPacksTotalCount)
+    const pageCount = useAppSelector(state => state.pack.pageCount)// количество элементов на странице
+    const page = useAppSelector(state => state.pack.page) // выбранная страница
+    const cardsPacksTotalCount = useAppSelector(state => state.pack.cardPacksTotalCount)// количество колод
     const user_id = useAppSelector(state => state.auth.data._id)
     const owner = useAppSelector(state => state.pack.owner)
 
@@ -159,13 +143,12 @@ export default function PackTable3() {
     React.useEffect(() => {
         let variable;
         if (owner === "my") {
-             variable = user_id
+            variable = user_id
         } else variable = ''
 
         dispatch(getPacksTC(pageCount, page, variable))
         // dispatch(getPacksTC())
     }, [pageCount, page, owner])
-
 
     const handleRequestSort = (
         event: React.MouseEvent<unknown>,
@@ -176,6 +159,35 @@ export default function PackTable3() {
         setOrderBy(property);
     };
 
+    const handleSelectAllClick = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event.target.checked)
+        if (event.target.checked) {
+            const newSelected = packs.map((n) => n.name);
+            setSelected(newSelected);
+            return;
+        }
+        setSelected([]);
+    };
+
+    // const handleClick = (event: React.MouseEvent<unknown>, name: string) => {
+    //     const selectedIndex = selected.indexOf(name);
+    //     let newSelected: readonly string[] = [];
+    //
+    //     if (selectedIndex === -1) {
+    //         newSelected = newSelected.concat(selected, name);
+    //     } else if (selectedIndex === 0) {
+    //         newSelected = newSelected.concat(selected.slice(1));
+    //     } else if (selectedIndex === selected.length - 1) {
+    //         newSelected = newSelected.concat(selected.slice(0, -1));
+    //     } else if (selectedIndex > 0) {
+    //         newSelected = newSelected.concat(
+    //             selected.slice(0, selectedIndex),
+    //             selected.slice(selectedIndex + 1),
+    //         );
+    //     }
+    //
+    //     setSelected(newSelected);
+    // };
 
 //Pagination
     const handleChangePage = (event: unknown, newPage: number) => {
@@ -189,15 +201,10 @@ export default function PackTable3() {
         // setPage(0);
         dispatch(setPageCount(parseInt(event.target.value)))
     };
-
-    // const handleChangeDense = (event: React.ChangeEvent<HTMLInputElement>) => {
-    //     setDense(event.target.checked);
-    //     console.log('handleChangeDense' + event.target.checked)
-    // };
-
-    const deletePackHandler = (idPack:string) => {
+    const deletePackHandler = (idPack: string) => {
         dispatch(removePackTC(idPack))
     }
+
 
     const isSelected = (name: string) => selected.indexOf(name) !== -1;
 
@@ -222,8 +229,16 @@ export default function PackTable3() {
                     <Table
                         sx={{minWidth: 750}}
                         aria-labelledby="tableTitle"
-                        size={dense ? 'small' : 'medium'}
+                        // size={dense ? 'small' : 'medium'}
                     >
+                        <EnhancedTableHead
+                            numSelected={selected.length}
+                            order={order}
+                            orderBy={orderBy}
+                            onSelectAllClick={handleSelectAllClick}
+                            onRequestSort={handleRequestSort}
+                            rowCount={packs.length}
+                        />
                         <TableBody>
                             {/* if you don't need to support IE11, you can replace the `stableSort` call with:
               rows.sort(getComparator(order, orderBy)).slice() */}
@@ -236,11 +251,12 @@ export default function PackTable3() {
 
                                         <TableRow
                                             hover
-                                            role="checkbox"
-                                            aria-checked={isItemSelected}
-                                            tabIndex={-1}
-                                            key={row.name}
-                                            selected={isItemSelected}
+                                            // onClick={(event) => handleClick(event, row.name)}
+                                            // role="checkbox"
+                                            // aria-checked={isItemSelected}
+                                            // tabIndex={-1}
+                                            // key={row.name}
+                                            // selected={isItemSelected}
                                         >
                                             <TableCell padding="checkbox"></TableCell>
                                             <TableCell
@@ -255,10 +271,15 @@ export default function PackTable3() {
                                             <TableCell align="right">{row.created}</TableCell>
                                             <TableCell align="right">{row.user_name}</TableCell>
                                             <TableCell align="right">
-                                                <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                                                    <Button onClick={()=> {console.log(row._id)}}>Learn</Button>
+                                                <ButtonGroup variant="contained"
+                                                             aria-label="outlined primary button group">
+                                                    <Button onClick={() => {
+                                                        console.log(row._id)
+                                                    }}>Learn</Button>
                                                     <Button>Edit</Button>
-                                                    <Button onClick={()=> {deletePackHandler(row._id)}}>Delete</Button>
+                                                    <Button onClick={() => {
+                                                        deletePackHandler(row._id)
+                                                    }}>Delete</Button>
                                                 </ButtonGroup>
                                             </TableCell>
                                         </TableRow>
@@ -266,15 +287,6 @@ export default function PackTable3() {
                                     </>
                                 );
                             })}
-                            {emptyRows > 0 && (
-                                <TableRow
-                                    style={{
-                                        height: (dense ? 33 : 53) * emptyRows,
-                                    }}
-                                >
-                                    <TableCell colSpan={6}/>
-                                </TableRow>
-                            )}
                         </TableBody>
                     </Table>
                 </TableContainer>
